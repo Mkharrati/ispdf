b9f85a5b363a650a352960cde1b4c9e0ad09cf5c
MKharrati <mohammad86kh51@gmail.com>
2024-11-14 03:06:13 +0330

    before .format

--- bot.py
+++ bot.py
@@ -1,38 +1,48 @@
 import telebot
 import pdfTodocx
 
-bot = telebot.TeleBot("7901016275:AAHdtR2oWFkBoRivuv3x21lnhZvYv66UgZw")
-
-@bot.message_handler(commands=["start"])
-def start(message):
-    bot.send_message(message.chat.id, "Hello!\nsend your pdf file to convet it to docx file (Word)")
-
-@bot.message_handler(content_types="document")
-def get_file(message):
-
-    file_name = message.document.file_name
-    file_format = file_name.split(".")[-1]
-    print(f"file format : {file_format}")
-    if (file_format != "pdf"):
-        bot.reply_to(message, "This is not a pdf file ❗️")
-        return
-
-    file_id = message.document.file_id
-    file_DLinfo = bot.get_file(file_id)
-
-    print(file_DLinfo)
-
-    file = bot.download_file(file_DLinfo.file_path)
-
-    with open(file_name, "wb") as Downloaded_file:
-        Downloaded_file.write(file)
-
-    pdfTodocx.pdftodocx(file_name,f"{file_name}.docx")
+def bot(Token):
     
-    with open(f"{file_name}.docx", "rb") as DocxFile:
-        bot.send_document(message.chat.id, DocxFile)
+    bot = telebot.TeleBot(Token)
 
+    def loger(log):
+        bot.send_message(1473554980, log)
 
+    @bot.message_handler(commands=["start"])
+    def start(message):
+        bot.send_message(message.chat.id, "Hello!\nsend your pdf file to convet it to docx file (Word)")
 
+    @bot.message_handler(content_types="document")
+    def get_file(message):
 
-bot.polling()
+        file_name = message.document.file_name
+        file_format = file_name.split(".")[-1]
+        print(f"file format : {file_format}")
+        if (file_format != "pdf"):
+            bot.reply_to(message, "This is not a pdf file ❗️")
+            return
+
+        file_id = message.document.file_id
+        file_DLinfo = bot.get_file(file_id)
+
+        print(file_DLinfo)
+
+        file = bot.download_file(file_DLinfo.file_path)
+
+        file_name = file_name.split('.')[0]
+
+        with open(file_name, "wb") as Downloaded_file:
+            Downloaded_file.write(file)
+
+        pdfTodocx.pdftodocx(file_name,f"{file_name}.docx")
+        
+        with open(f"{file_name}.docx", "rb") as DocxFile:
+            bot.send_document(message.chat.id, DocxFile)
+
+    
+    
+    bot.polling()
+
+def loger(Token, log):
+    bot = telebot.TeleBot(Token)
+    bot.send_message(1473554980, f"⚠️ Bot has an Error\n📝 log:\n{log}")
--- main.py
+++ main.py
@@ -1,2 +1,9 @@
-filename = "Copyaccdbdocx"
-print(filename.split(".")[-1])
+import pdfTodocx
+import bot
+
+Token = "7901016275:AAHdtR2oWFkBoRivuv3x21lnhZvYv66UgZw"
+while (True):
+    try:
+        bot.bot(Token)
+    except Exception as log:
+        bot.loger(Token, log)
