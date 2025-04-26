fd0fff62812793b3e42fa0629a92fb49681c85e1
MKharrati <mohammad86kh51@gmail.com>
2025-04-26 08:51:16 +0330

    refactor: Initialize messages module

---
+++ messages/messages.py
@@ -0,0 +1,83 @@
+class EnglishMessages:
+    START = "Hello!\nChoose:"
+
+    PLEASE_WAIT = "Please wait ..."
+
+    REQ_PHOTO = "Send your photos : "
+
+    PHOTO_ONLY = "Please just send a <b>photo</b>."
+
+    REQ_PDF = "Please send your PDF file :"
+
+    PDF_ONLY = "Please send PDF file only ❗️"
+
+    FILE_SIZE_ERROR_20MB = "Please send a file that's less than 20 MB ❗️"
+
+    INVALID_PDF = "Please send a valid PDF file"
+
+    MAX_PDF_PAGES_10 = "Maximum PDF page limit: 10 pages."
+
+    REQ_WORD = "Please send your Word file :"
+
+    WORD_ONLY = "Please send docx file only ❗️"
+
+    REQ_FILE = "Please send your file :"
+
+    FILE_ONLY = "Please send file only ❗️"
+
+    INVALID_CONTENT = "Content type is invalid ❗️"
+
+    REQ_NEWNAME = "Enter new name :"
+
+    TEXT_ONLY = "Please send text only ❗️"
+
+    REQ_PPTX = "Please send your PowerPoint file :"
+
+    PPTX_ONLY = "Please send .pptx file only ❗️"
+
+    INVALIDE_FILE = "The file is invalid ❗️"
+
+    DOC_ONLY = "Please send documents only ❗️"
+
+    OCCURRED_ERROR = "An error occurred ❗️ Try again later."
+
+    REQ_TEXT = "Please Send your text :"
+
+    TEXT_TOO_LONG = "The text is too long ❗️"
+
+    #Buttons
+    _IMAGE_PDF = "Image To PDF"
+
+    _WORD_PDF = "Word to PDF"
+
+    _TEXT_PDF = "Text to pdf"
+
+    _PDF_WORD = "PDF to Word"
+
+    _IMAGE_TEXT = "Image to text"
+
+    _UNLOCK_PDF = "Unlock PDF"
+
+    _PDF_IMAGE = "PDF to image"
+
+    _MERGE_PDF = "Merge PDF"
+
+    _RENAME_FILE = "Rename File"
+    
+    _PPT_PDF = "Powerpoint to pdf"
+
+    _QRCODE = "QRcode"
+
+    _BACK = "Back"
+
+    _FINISH = "Finish"
+
+
+
+
+
+
+
+
+
+
